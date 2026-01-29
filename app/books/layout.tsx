29cdf5afe696fa3061c90bf80eb429f0ae13e486
Allen Lee <lseconomist@gmail.com>
2026-01-30 04:38:41 +0800

    re-headed

--- app/books/layout.tsx
+++ app/books/layout.tsx
@@ -6,7 +6,7 @@ export default function BooksLayout({
 	return (
 		<main
 			id="main"
-			className="mx-auto flex max-w-[90%] grow flex-col items-center pt-16 lg:max-w-[60%]"
+			className="mx-auto flex w-full max-w-[90%] grow flex-col items-center pt-16 lg:w-[900px]"
 		>
 			{children}
 		</main>

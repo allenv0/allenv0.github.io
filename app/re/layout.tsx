201479f1b7b1f7313c58a54bf26fe41546329736
Allen Lee <lseconomist@gmail.com>
2026-01-30 04:19:59 +0800

    re-headed

---
+++ app/re/layout.tsx
@@ -0,0 +1,14 @@
+export default function ReLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<main
+			id="main"
+			className="mx-auto flex max-w-[90%] grow flex-col items-center lg:max-w-[60%]"
+		>
+			{children}
+		</main>
+	);
+}
